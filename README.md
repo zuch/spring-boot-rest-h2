cfaa960bc7aadf701c3781ec5731e6d7e4110e04
Sascha Watermeyer <sascha@travelstart.com>
2019-08-14 22:06:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spring Boot REST H2
+
+Spring Boot 2 REST service with JPA, Lombok and H2 Database
+
+
---
+++ src/main/java/com/github/zuch/SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.zuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestApplication.class, args);
+    }
+
+}
